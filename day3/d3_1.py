5d6d5a0077b3ad39cfa9995f6ff1c48acf1c26c9
Diana <kitkhadiana>
2022-12-03 12:56:45 +0000

    one liner bruh

--- day3/d3_1.py
+++ day3/d3_1.py
@@ -1,17 +1,7 @@
-f = open("day3/d3_input.txt", "r")
-
 def getPriority(char):
   if char.islower():
     return ord(char) - 96
   return ord(char) - 38
 
-s = 0
-for x in f:
-  a = set(x[:len(x) // 2])
-  b = set(x[len(x) // 2 :].rstrip())
-  common = a&b
-  for c in common:
-    s += getPriority(c)
-
-print(s)
-f.close()
+with open("day3/d3_input.txt", "r") as f:
+  print(sum(getPriority((set(x[:len(x) // 2]) & set(x[len(x) // 2:].rstrip())).pop()) for x in f))
