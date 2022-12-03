812d43cdcd1b4706631cea407b7c4d579c0240b6
Diana <kitkhadiana>
2022-12-03 12:58:36 +0000

    file, but in the right way

--- day3/d3_1.py
+++ day3/d3_1.py
@@ -3,5 +3,16 @@ def getPriority(char):
     return ord(char) - 96
   return ord(char) - 38
 
+# one liner
 with open("day3/d3_input.txt", "r") as f:
   print(sum(getPriority((set(x[:len(x) // 2]) & set(x[len(x) // 2:].rstrip())).pop()) for x in f))
+
+# multi liner
+with open("day3/d3_input.txt", "r") as f:
+  s = 0
+  for x in f:
+    a = set(x[:len(x) // 2])
+    b = set(x[len(x) // 2 :].rstrip())
+    common = a&b
+    s += getPriority(common.pop())
+  print(s)
